cb4a9bc157e76f63edcdf8c15e7d522c53377227
ESTHEREKANEM <estekanem1@gmail.com>
2023-02-14 12:31:33 -0800

    SQL

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- This lists all databases of your MySQL SERVER.
+
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- creating the databases hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 already exists, your script should not fail
+-- You are not allowed to use the SELECTION or SHOW statements
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

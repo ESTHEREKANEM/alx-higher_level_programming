d896b3eb8a5ee72946136bfd06b71aec6fd9a2c7
ESTHEREKANEM <estekanem1@gmail.com>
2023-03-14 09:13:28 -0700

    Updated

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+!/usr/bin/node
+// Checks the parameters provided
+
+class Rectangle {
+  constructor (width, height) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+}
+module.exports = Rectangle;

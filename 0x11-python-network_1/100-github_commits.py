7388cc47c91ff277703d5f478765cf1dc0da773c
ESTHEREKANEM <estekanem1@gmail.com>
2023-03-31 14:26:29 -0700

    Updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) of the repository and user
-sent in as arguments
+Lists 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”.
+Usage: ./list_commits.py <repository name> <owner name>
 """
+import sys
+import requests
+
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[2], argv[1]))
-    commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author_name}")
+    else:
+        print(f"Error: {response.status_code} - {response.json().get('message')}")
 

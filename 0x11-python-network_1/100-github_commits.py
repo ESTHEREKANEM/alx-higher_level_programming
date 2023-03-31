87e05c96464e4870e73130c08b95c190af8fbe83
ESTHEREKANEM <estekanem1@gmail.com>
2023-03-31 14:33:01 -0700

    up to date

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,16 +7,24 @@ import sys
 import requests
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Error: Invalid number of arguments")
+        print("Usage: ./list_commits.py <repository name> <owner name>")
+        sys.exit(1)
+
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
+
     url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
     response = requests.get(url)
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print(f"{sha}: {author_name}")
-    else:
+
+    if response.status_code != 200:
         print(f"Error: {response.status_code} - {response.json().get('message')}")
+        sys.exit(1)
+
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
 

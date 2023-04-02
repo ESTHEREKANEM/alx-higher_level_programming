ab8c4830d5cd5277957364c56cf406cb50f0d981
Ekanem, Uduakabasi Ekere <111003966+ESTHEREKANEM@users.noreply.github.com>
2023-04-02 20:50:54 +0100

    Update hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,8 @@
 """Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    response = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")

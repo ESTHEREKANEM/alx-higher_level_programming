caf22f3ffb6043755be33cbba18186cb972cbca6
ESTHEREKANEM <estekanem1@gmail.com>
2023-04-25 15:30:19 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-// Reads and prints the content of a file
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'ut8', function (err data {
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.log(err)
-  } else {
-    process.stdout.write(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
